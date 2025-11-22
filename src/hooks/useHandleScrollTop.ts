6344cb94bd517c07c34cad6424bd704f86fe39ce
mohdkhiti-web <mkhiti@iscaa.sabis.net>
2025-11-22 11:03:48 +0400

    d

--- src/hooks/useHandleScrollTop.ts
+++
@@ -1,6 +0,0 @@
-export const useHandleScrollTop = () => {
-  const handleScrollTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-  return handleScrollTop;
-};
--- src/types/Benefits.ts
+++
@@ -1,5 +0,0 @@
-export type BenefitsProps = {
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-  title: string;
-  description: string;
-};
